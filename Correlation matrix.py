107d5af253c6140a669476a94a6a1913bf3c58a5
Nayssi <33839463+Nayssi@users.noreply.github.com>
2017-11-26 04:04:02 +0100

    Update Correlation matrix.py

--- Correlation matrix.py
+++ Correlation matrix.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-#to read on your computer, change the address#
 jd=pd.read_csv("C:/Users/drh/Desktop/XFC/jd.csv")
 baba=pd.read_csv("C:/Users/drh/Desktop/XFC/baba.csv")
 ttwo=pd.read_csv("C:/Users/drh/Desktop/XFC/ttwo.csv")
@@ -15,10 +14,10 @@ for i in range(n-1):
     daily_return=pd.merge(daily_return,portfolio[i+1],on="Date")
 
 #from prices to returns
-daily_return=daily_return.iloc[:,1:n+1].pct_change(1)
+daily_return=daily_return.iloc[:,1:n+1].pct_change(1).iloc[1:,:]
  
 #returns observation period
-T=daily_return.shape[0]-1 
+T=daily_return.shape[0]
  
 #demean returns then divide them by cross-sectional vol and sample vol to normalize#
 daily_return=daily_return.sub(daily_return.mean(),axis=1)
@@ -26,6 +25,6 @@ daily_return=daily_return.div(daily_return.std(axis=1),axis=0)
 daily_return=daily_return.div(daily_return.std(axis=0),axis=1)
 
 #Naive estimator that we're going to clean#
-estimator=(1/T)*daily_return.dot(daily_return.transpose())
+estimator=(1/T)*daily_return.transpose().dot(daily_return)
     
     
